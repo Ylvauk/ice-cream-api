34be909149beb0028e4087ad145415e629ec5567
esin87 <esin.saribudak@gmail.com>
2022-01-20 21:43:00 -0600

    Update for MongoDB Atlas and Mongoose v6

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+// Create a .env file and paste your DATABASE_URL in it to connect your MongoDB Atlas database
+// Remember to restart your server if needed
+const mongoURI = process.env.DATABASE_URL;
 
-mongoose.connect('mongodb://localhost/icecream', { useNewUrlParser: true, useFindAndModify: false })
-.then((conn) => {
-	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
-})
-.catch(err => {
-	console.error(err)
-})
+mongoose
+	.connect(mongoURI)
+	.then((conn) => {
+		console.log(
+			`Connected to MongoDB Atlas on ${conn.connections[0].name} database. Life is good 😎`
+		);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
 
-
-module.exports = mongoose
+module.exports = mongoose;
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,13 @@
-const mongoose = require('../db/connection')
-const seedData = require('./seeds.json')
-const IceCream = require('./models/IceCream')
+const mongoose = require('../db/connection');
+const seedData = require('./seeds.json');
+const IceCream = require('./models/IceCream');
 
-IceCream.deleteMany({}).then(() => {
-    IceCream.insertMany(seedData).then((iceCream) => {
-        console.log(iceCream)
-        process.exit()
-    })
-}).catch((err) => console.error(err))
+IceCream.deleteMany({})
+	.then(() => {
+		IceCream.insertMany(seedData).then((iceCream) => {
+			console.log('We have ice creams 😋😋😋');
+			console.log(iceCream);
+			process.exit();
+		});
+	})
+	.catch((err) => console.error(err));
